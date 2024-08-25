d93b76561844d4a84d3c9d76123e4aba8cec44f3
Le Vivilet <levivlet@pm.me>
2024-08-25 16:12:20 +0200

    remove unused

--- src/parts/ProcessExplorerFrontendIpc/ProcessExplorerFrontendIpc.js
+++ src/parts/ProcessExplorerFrontendIpc/ProcessExplorerFrontendIpc.js
@@ -1,16 +1,6 @@
-import * as JsonRpc from '../JsonRpc/JsonRpc.js'
-
 export const state = {
   /**
    * @type {any}
    */
   ipc: undefined,
-}
-
-const send = (method, ...params) => {
-  const { ipc } = state
-  if (!ipc) {
-    return
-  }
-  JsonRpc.send(ipc, method, ...params)
 }
--- src/parts/WindowsProcessTreeDataFlag/WindowsProcessTreeDataFlag.js
+++ src/parts/WindowsProcessTreeDataFlag/WindowsProcessTreeDataFlag.js
@@ -1,3 +1,2 @@
-const None = 0
 export const Memory = 1
 export const CommandLine = 2
