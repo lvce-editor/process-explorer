cd2100093df6003bc9e4664e183bf6ffc7c5b048
Le Vivilet <levivlet@pm.me>
2024-08-31 09:17:05 +0200

    tsc

--- src/parts/WindowsProcessTree/WindowsProcessTree.ts
+++ src/parts/WindowsProcessTree/WindowsProcessTree.ts
@@ -1,16 +1,16 @@
+import type { IProcessInfo } from '@vscode/windows-process-tree'
 import * as LoadWindowsProcessTree from '../LoadWindowsProcessTree/LoadWindowsProcessTree.ts'
 import * as Promises from '../Promises/Promises.ts'
 
-/**
- *
- * @param {number} rootPid
- * @param {number} flags
- * @returns {Promise<any[] | undefined>}
- */
-export const getProcessList = async (rootPid, flags) => {
+export const getProcessList = async (
+  rootPid: number,
+  flags: number,
+): Promise<IProcessInfo[] | undefined> => {
   const WindowsProcessTree =
     await LoadWindowsProcessTree.loadWindowProcessTree()
-  const { resolve, promise } = Promises.withResolvers()
+  const { resolve, promise } = Promises.withResolvers<
+    IProcessInfo[] | undefined
+  >()
   WindowsProcessTree.getProcessList(rootPid, resolve, flags)
   return promise
 }
