2aa7e277d9274bb2e0cb16834a2b3703fae063b4
Le Vivilet <levivilet@pm.me>
2026-06-30 00:34:53 +0200

    fix: lint (#23)

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,12 +1,12 @@
 import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.ts'
-import * as ProcessId from '../ProcessId/ProcessId.ts'
 import * as ListProcessesWithMemoryUsage from '../ListProcessesWithMemoryUsage/ListProcessesWithMemoryUsage.ts'
+import * as ProcessId from '../ProcessId/ProcessId.ts'
 
 export const commandMap = {
   'HandleElectronMessagePort.handleElectronMessagePort':
     HandleElectronMessagePort.handleElectronMessagePort,
-  'ProcessId.getMainProcessId': ProcessId.getMainProcessId,
   'ListProcessesWithMemoryUsage.listProcessesWithMemoryUsage':
     ListProcessesWithMemoryUsage.listProcessesWithMemoryUsage,
+  'ProcessId.getMainProcessId': ProcessId.getMainProcessId,
   // 'ElectronContextMenu.openContextMenu': ElectronWebContentsView.handleContextMenu,
 }
--- src/parts/IpcChildModule/IpcChildModule.ts
+++ src/parts/IpcChildModule/IpcChildModule.ts
@@ -9,14 +9,14 @@ import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
 
 export const getModule = (method) => {
   switch (method) {
+    case IpcChildType.ElectronMessagePort:
+      return IpcChildWithElectronMessagePort
+    case IpcChildType.ElectronUtilityProcess:
+      return IpcChildWithElectronUtilityProcess
     case IpcChildType.NodeForkedProcess:
       return IpcChildWithNodeForkedProcess
     case IpcChildType.NodeWorker:
       return IpcChildWithNodeWorker
-    case IpcChildType.ElectronUtilityProcess:
-      return IpcChildWithElectronUtilityProcess
-    case IpcChildType.ElectronMessagePort:
-      return IpcChildWithElectronMessagePort
     case IpcChildType.WebSocket:
       return IpcChildWithWebSocket
     default:
