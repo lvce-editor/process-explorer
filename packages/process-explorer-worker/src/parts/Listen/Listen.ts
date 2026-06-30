0d346f338fe85941804985d5e8440c6d0f6e740b
Le Vivilet <levivilet@pm.me>
2026-06-30 12:35:17 +0200

    lint

--- packages/process-explorer-worker/src/parts/Listen/Listen.ts
+++ packages/process-explorer-worker/src/parts/Listen/Listen.ts
@@ -1,9 +1,10 @@
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 import { registerCommands } from '../ProcessExplorerStates/ProcessExplorerStates.ts'
 import { initializeRendererWorker } from '../InitializeRendererWorker/initializeRendereWorker.ts'
+import { initializeFileSystemWorker } from '../InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
 
 export const listen = async (): Promise<void> => {
   registerCommands(CommandMap.commandMap)
 
-  await Promise.all([initializeRendererWorker()])
+  await Promise.all([initializeRendererWorker(), initializeFileSystemWorker()])
 }
