5e9dc87970c466ea775c757f3fc0c5d1d520b86d
Le Vivilet <levivilet@pm.me>
2026-07-01 10:10:54 +0200

    fix

--- packages/process-explorer/src/parts/CommandMap/CommandMap.ts
+++ packages/process-explorer/src/parts/CommandMap/CommandMap.ts
@@ -1,6 +1,7 @@
 import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.ts'
 import * as HandleMessagePort from '../HandleMessagePort/HandleMessagePort.ts'
 import * as HandleSocket from '../HandleSocket/HandleSocket.ts'
+import * as HandleWebSocket from '../HandleWebSocket/HandleWebSocket.ts'
 import * as ListProcessesWithMemoryUsage from '../ListProcessesWithMemoryUsage/ListProcessesWithMemoryUsage.ts'
 import * as ProcessId from '../ProcessId/ProcessId.ts'
 
@@ -9,6 +10,7 @@ export const commandMap = {
     HandleElectronMessagePort.handleElectronMessagePort,
   'HandleMessagePort.handleMessagePort': HandleMessagePort.handleMessagePort,
   'HandleSocket.handleSocket': HandleSocket.handleSocket,
+  'HandleWebSocket.handleWebSocket': HandleWebSocket.handleWebSocket,
   'ListProcessesWithMemoryUsage.listProcessesWithMemoryUsage':
     ListProcessesWithMemoryUsage.listProcessesWithMemoryUsage,
   'ProcessId.getMainProcessId': ProcessId.getMainProcessId,
