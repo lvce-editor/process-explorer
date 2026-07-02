6554e155d9898273decbe2ecb79fea53b4cc1867
Le Vivilet <levivilet@pm.me>
2026-07-02 15:25:54 +0200

    feature: enable open e2e test

--- packages/e2e/src/viewlet.process-explorer.open.ts
+++ packages/e2e/src/viewlet.process-explorer.open.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.process-explorer.open'
 
-export const skip = 1
+// export const skip = 1
 
 export const test: Test = async ({ Command, expect, Locator }) => {
   // act
