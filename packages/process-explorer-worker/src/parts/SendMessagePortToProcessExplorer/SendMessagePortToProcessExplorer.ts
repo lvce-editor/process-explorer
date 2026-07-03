e92c865f22a4b3966a89deceeefb963f28f18c9c
Le Vivilet <levivilet@pm.me>
2026-07-03 22:05:57 +0200

    fix: send process explorer message port

--- packages/process-explorer-worker/src/parts/SendMessagePortToProcessExplorer/SendMessagePortToProcessExplorer.ts
+++ packages/process-explorer-worker/src/parts/SendMessagePortToProcessExplorer/SendMessagePortToProcessExplorer.ts
@@ -1,14 +1,7 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-interface RendererWorkerWithProcessExplorer {
-  readonly sendMessagePortToProcessExplorer: (
-    port: MessagePort,
-  ) => Promise<void>
-}
-
 export const sendMessagePortToProcessExplorer = async (
   port: MessagePort,
 ): Promise<void> => {
-  const rendererWorker = RendererWorker
-  await rendererWorker.toproc(port)
+  await RendererWorker.sendMessagePortToProcessExplorer(port)
 }
