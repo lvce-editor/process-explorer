9cf1efc44f385ef1bb09941be16910be4f4983c8
Le Vivilet <levivilet@pm.me>
2026-07-22 20:57:07 +0000

    feature: send message port to main process

--- packages/process-explorer-worker/src/parts/SendMessagePortToProcessExplorer/SendMessagePortToProcessExplorer.ts
+++
@@ -1,7 +0,0 @@
-import { RendererWorker } from '@lvce-editor/rpc-registry'
-
-export const sendMessagePortToProcessExplorer = async (
-  port: MessagePort,
-): Promise<void> => {
-  await RendererWorker.sendMessagePortToProcessExplorer(port)
-}
