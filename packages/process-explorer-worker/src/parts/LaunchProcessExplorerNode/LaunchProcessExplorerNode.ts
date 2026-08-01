0b4d7c898f205e16028373abc45c7804ebb6080f
Le Vivilet <levivilet@pm.me>
2026-08-01 02:33:00 -0700

    feature: request websocket capabilities (#119)

--- packages/process-explorer-worker/src/parts/LaunchProcessExplorerNode/LaunchProcessExplorerNode.ts
+++ packages/process-explorer-worker/src/parts/LaunchProcessExplorerNode/LaunchProcessExplorerNode.ts
@@ -1,11 +1,38 @@
 import type { Rpc } from '@lvce-editor/rpc'
-import { LazyWebSocketRpcParent2 } from '@lvce-editor/rpc'
+import { LazyWebSocketRpcParent2, WebSocketRpcParent } from '@lvce-editor/rpc'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import { VError } from '@lvce-editor/verror'
+
+const commandNotFoundRegex = /command not found|not found/i
 
 export const launchProcessExplorerNode = async (
   onClose: () => void,
 ): Promise<Rpc> => {
   try {
+    try {
+      const { protocols, url } = (await RendererWorker.invoke(
+        'WebSocketCapability.create',
+        'process-explorer',
+      )) as {
+        readonly protocols: string[]
+        readonly url: string
+      }
+      const webSocket = new WebSocket(url, protocols)
+      webSocket.addEventListener('close', onClose)
+      return await WebSocketRpcParent.create({
+        commandMap: {},
+        webSocket,
+      })
+    } catch (error) {
+      if (!(
+        error instanceof Error &&
+        (error.message.includes('WebSocketCapability.create') ||
+          error.message.includes('module WebSocketCapability not found')) &&
+        commandNotFoundRegex.test(error.message)
+      )) {
+        throw error
+      }
+    }
     const rpc = await LazyWebSocketRpcParent2.create({
       commandMap: {},
       onClose,
