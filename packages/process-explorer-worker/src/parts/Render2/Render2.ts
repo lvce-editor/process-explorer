5af55d4221c6ad7f6361dda471add89ef6f0b920
Le Vivilet <levivilet@pm.me>
2026-08-15 13:29:39 +0000

    feature: render directly in renderer process

--- packages/process-explorer-worker/src/parts/Render2/Render2.ts
+++ packages/process-explorer-worker/src/parts/Render2/Render2.ts
@@ -1,11 +1,21 @@
 import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as ProcessExplorerStates from '../ProcessExplorerStates/ProcessExplorerStates.ts'
+import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
 
 export const render2 = (
   uid: number,
   diffResult: readonly number[],
-): readonly any[] => {
+): readonly any[] | Promise<readonly any[]> => {
   const { oldState, scheduledState } = ProcessExplorerStates.get(uid)
   ProcessExplorerStates.set(uid, scheduledState, scheduledState)
-  return ApplyRender.applyRender(oldState, scheduledState, diffResult)
+  const commands = ApplyRender.applyRender(oldState, scheduledState, diffResult)
+  if (!RendererProcess.isConnected()) return commands
+  return renderDirect(uid, commands)
+}
+
+const renderDirect = async (uid: number, commands: readonly any[]): Promise<readonly any[]> => {
+  const rendererWorkerCommands = commands.filter((command) => command[0] === 'Viewlet.setFocusContext')
+  const rendererProcessCommands = commands.filter((command) => command[0] !== 'Viewlet.setFocusContext')
+  const transactionId = await RendererProcess.invoke('Viewlet.queueCommands', uid, rendererProcessCommands)
+  return [...rendererWorkerCommands, ['Viewlet.commitPending', uid, transactionId]]
 }
